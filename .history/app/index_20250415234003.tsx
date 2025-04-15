4821214f670f0251235895104c86cf69f8f93de7
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-04-15 23:53:13 +0300

    fixed

--- .history/app/index_20250415234003.tsx
+++ .history/app/index_20250415234003.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className=" bg-red-500 font-bold ">
+    <View className=" bg-red-500">
       <Text>NativeWind is working!</Text>
     </View>
   );
--- .history/app/index_20250415235259.tsx
+++ .history/app/index_20250415235259.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className=" bg-red-500 font-bold ">
+    <View className=" bg-red-500 font-bold size-7">
       <Text>NativeWind is working!</Text>
     </View>
   );
